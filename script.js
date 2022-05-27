df2a023a85fa0b3bfbfe2dd9c94ca85fa8d36508
Nicholas Gillespie <contact@nicholasgillespie.com>
2022-05-27 10:30:05 +0200

    format dates & display dates passed on movements

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-05-20T17:01:17.194Z',
+    '2022-05-26T16:36:17.929Z',
+    '2022-05-27T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'fr-FR',
@@ -116,6 +116,24 @@ const displaySummary = function (account) {
   labelSumInterest.textContent = `${interest.toFixed(2)} €`;
 };
 
+// TIME
+const formatMovementDate = function (date) {
+  // calculate days between now and date of movement
+  const calcDaysPassed = function (date1, date2) {
+    return Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  };
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  if (daysPassed === 0) return `Today`;
+  if (daysPassed === 1) return `Yesterday`;
+  if (daysPassed <= 7) return `${daysPassed} Days ago`;
+  // if not captured by conditions with return, the following happens:
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 // DISPLAY MOVEMENTS & SORT
 const displayMovements = function (account, sort = false) {
   // innerHTML sets the HTML markup contained within the element
@@ -130,11 +148,7 @@ const displayMovements = function (account, sort = false) {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(account.movementsDates[index]);
-
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="[ box cluster ][ movement ]">
@@ -308,3 +322,22 @@ const bigDeposits = accounts
   .flatMap((accounts) => accounts.movements)
   .filter((mov) => mov >= 1000).length;
 // console.log(bigDeposits);
+
+// const bday = new Date(' Feb 24 1986');
+// const age = Date.now() - bday;
+// console.log(age.toISOString());
+
+// const now = Date.now();
+// const bday = new Date('24 Feb 1986').getTime();
+
+// console.log(new Date());
+
+// console.log(now);
+// console.log(bday);
+
+// const daysPassed = function (now, date) {
+//   return (now - date) / (1000 * 60 * 60 * 24 * 365);
+// };
+
+// const result = daysPassed(now, bday);
+// console.log(`Days passed since : ${result}`);
